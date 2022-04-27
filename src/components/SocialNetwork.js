24dbd224399e26d406ff97452155aef25951f361
Elisee-des <esabidani@gmail.com>
2022-04-27 19:09:22 +0000

    composant _DynamicText ok

---
+++ src/components/SocialNetwork.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const SocialNetwork = () => {
+    return (
+        <div className="social-network">
+            <ul className="content">
+                <a href="" className="hover">
+                    <li>
+                        <i className="fab fa-facebook"></i>
+                    </li>
+                </a>
+                <a href="" className="hover">
+                    <li>
+                        <i className="fab fa-twitter"></i>
+                    </li>
+                </a>
+                <a href="" className="hover">
+                    <li>
+                        <i className="fab fa-instagram"></i>
+                    </li>
+                </a>
+            </ul>
+        </div>
+    );
+};
+
+export default SocialNetwork;
