9636bd21b1c9b4f61f847e801bbeba4f5ebc1b0c
Elisee-des <esabidani@gmail.com>
2022-04-27 19:19:09 +0000

    debut composant Buttons

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div className="scroll-bottom">
+            <NavLink to="" className="left hover">
+                <span>&#10092;</span>
+            </NavLink>
+            <NavLink to="" className="right hover">
+                <span>&#10093;</span>
+            </NavLink>
+        </div>
+    );
+};
+
+export default Button;
